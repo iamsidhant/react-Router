df0f140f4d46e52d2ef7a7f7441fef1efa4a3e02
Sidhant Arya <121373417+iamsidhant@users.noreply.github.com>
2023-10-28 12:27:49 +0530

    react router

---
+++ reactRouter/src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import Header from './components/Header/Header'
+import Footer from './components/Footer/Footer'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <h1 className='bg-orange-500 p-6'>React router</h1>
+    </>
+  )
+}
+
+export default App
